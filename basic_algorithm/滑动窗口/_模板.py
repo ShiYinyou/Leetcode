2a5c82b685c429967fdd1a7f12e065b7af598a2c
ShiYinyou <1589355225@qq.com>
2024-11-22 15:55:01 +0800

    滑动窗口模板

---
+++ basic_algorithm/滑动窗口/_模板.py
@@ -0,0 +1,15 @@
+class Template:
+    def sliding_window(arr):
+        """
+        滑动窗口常用于解决一些涉及子串或自数组的问题
+        通过维护一个可变大小的窗口来遍历字符串或数组执行特定操作
+        根据题目要求，可分为：定长滑动窗口、不定长滑动窗口
+        关键词：字符串、数组、正数
+        """
+        left = s = 0
+        for right,x in enumerate(arr):
+            s += x
+            while case: # 判断需要收缩左窗口
+                s -= arr[left]
+                left += 1
+        return
